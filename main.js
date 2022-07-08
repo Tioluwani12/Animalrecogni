0d5bb1d3fe9cf75ad302a347bca72e15806b455e
Tioluwani12 <95766690+Tioluwani12@users.noreply.github.com>
2022-07-08 17:31:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/htpViHxyk/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResult);
+}
+function gotResult(error,results){
+    console.log('gotResult');
+}
